03a584bd089632d096886a8723376fa53ea64d72
daniel-liberto <daniel.liberto@hotmail.com>
2024-01-25 17:03:25 -0300

    first commit

---
+++ src/main/java/com/danielalmeida/springlesson/SpringlessonApplication.java
@@ -0,0 +1,15 @@
+package com.danielalmeida.springlesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlessonApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringlessonApplication.class, args);
+		
+	}
+
+}

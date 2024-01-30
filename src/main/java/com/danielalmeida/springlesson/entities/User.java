a3a4f34360e6521c7f063f852ddee2b0bff50b78
daniel-liberto <daniel.liberto@hotmail.com>
2024-01-30 11:20:31 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/danielalmeida/springlesson/entities/User.java
+++ src/main/java/com/danielalmeida/springlesson/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -18,6 +20,8 @@ public class User implements Serializable {
   private String email;
   private String phone;
   private String password;
+  @OneToMany(mappedBy = "client")
+  private List<Order> orders = new ArrayList<>();
 
   public User() {
   }
@@ -68,6 +72,10 @@ public class User implements Serializable {
 
   public String getPassword() {
     return password;
+  }
+
+  public List<Order> getOrders(){
+    return orders;
   }
 
   @Override

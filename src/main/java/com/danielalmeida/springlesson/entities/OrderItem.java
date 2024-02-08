5efe98dfc49a467ff03df0f9ce2fba7045932c9b
daniel-liberto <daniel.liberto@hotmail.com>
2024-02-07 22:24:32 -0300

    Subtotal and Total methods

--- src/main/java/com/danielalmeida/springlesson/entities/OrderItem.java
+++ src/main/java/com/danielalmeida/springlesson/entities/OrderItem.java
@@ -59,6 +59,10 @@ public class OrderItem implements Serializable {
     return price;
   }
 
+  public Double getSubTotal(){
+    return quantity * price;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
